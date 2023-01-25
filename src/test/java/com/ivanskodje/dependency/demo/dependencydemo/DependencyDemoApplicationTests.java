17f77114600dcb4b53a78520f4970a3141542575
Ivan Skodje <ivanskodje@protonmail.com>
2023-01-25 19:13:17 +0100

    first commit

---
+++ src/test/java/com/ivanskodje/dependency/demo/dependencydemo/DependencyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanskodje.dependency.demo.dependencydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
